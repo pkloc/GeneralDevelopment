5e40365b03bc5de940db0ef95145b4c08be263ef
phil_kloc@yahoo.ca <phil_kloc@yahoo.ca@4d5d91af-f081-4a24-a62b-0426189fedf5>
2016-07-27 14:58:36 +0000

    Initial commit

---
+++ Arduino/trunk/wifiDefault/wifiDefault.ino
@@ -0,0 +1,213 @@
+/************************************************************
+ESP8266_Shield_Demo.h
+SparkFun ESP8266 AT library - Demo
+Jim Lindblom @ SparkFun Electronics
+Original Creation Date: July 16, 2015
+https://github.com/sparkfun/SparkFun_ESP8266_AT_Arduino_Library
+
+This example demonstrates the basics of the SparkFun ESP8266
+AT library. It'll show you how to connect to a WiFi network,
+get an IP address, connect over TCP to a server (as a client),
+and set up a TCP server of our own.
+************************************************************/
+
+//////////////////////
+// Library Includes //
+//////////////////////
+// SoftwareSerial is required (even you don't intend on using it).
+#include <SoftwareSerial.h> 
+#include <SparkFunESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <Temboo.h>
+#include "TembooAccount.h" // Contains Temboo account information
+
+//////////////////////////////
+// WiFi Network Definitions //
+//////////////////////////////
+// Replace these two character strings with the name and
+// password of your WiFi network.
+const char mySSID[] = "attic";
+const char myPSK[] = "dupasalata";
+
+WiFiClient client;
+
+int numRuns = 1;   // Execution count, so this doesn't run forever
+int maxRuns = 3;   // Maximum number of times the Choreo should be executed
+
+// All functions called from setup() are defined below the
+// loop() function. They modularized to make it easier to
+// copy/paste into sketches of your own.
+void setup() 
+{
+  // Serial Monitor is used to control the demo and view
+  // debug information.
+  Serial.begin(9600);
+  serialTrigger(F("Press any key to begin."));
+
+  // initializeESP8266() verifies communication with the WiFi
+  // shield, and sets it up.
+  initializeESP8266();
+
+  // connectESP8266() connects to the defined WiFi network.
+  connectESP8266();
+
+  // displayConnectInfo prints the Shield's local IP
+  // and the network it's connected to.
+  displayConnectInfo();
+
+  Serial.println("Setup complete.\n");
+
+  serialTrigger(F("Press any key to connect client."));  
+}
+
+void loop() 
+{
+   getWeather();
+   
+   Serial.println("\nWaiting...\n");
+   delay(10000); // wait 10 seconds between GetWeatherByAddress calls
+}
+
+void initializeESP8266()
+{
+  // esp8266.begin() verifies that the ESP8266 is operational
+  // and sets it up for the rest of the sketch.
+  // It returns either true or false -- indicating whether
+  // communication was successul or not.
+  // true
+  bool success = esp8266.begin();
+  if (!esp8266.begin())
+  {
+    Serial.println(F("Error talking to ESP8266."));
+    errorLoop(0);
+  }
+  Serial.println(F("ESP8266 Shield Present"));
+}
+
+void connectESP8266()
+{
+  // The ESP8266 can be set to one of three modes:
+  //  1 - ESP8266_MODE_STA - Station only
+  //  2 - ESP8266_MODE_AP - Access point only
+  //  3 - ESP8266_MODE_STAAP - Station/AP combo
+  // Use esp8266.getMode() to check which mode it's in:
+  int retVal = esp8266.getMode();
+  if (retVal != ESP8266_MODE_STA)
+  { // If it's not in station mode.
+    // Use esp8266.setMode([mode]) to set it to a specified
+    // mode.
+    retVal = esp8266.setMode(ESP8266_MODE_STA);
+    if (retVal < 0)
+    {
+      Serial.println(F("Error setting mode."));
+      errorLoop(retVal);
+    }
+  }
+  Serial.println(F("Mode set to station"));
+
+  // esp8266.status() indicates the ESP8266's WiFi connect
+  // status.
+  // A return value of 1 indicates the device is already
+  // connected. 0 indicates disconnected. (Negative values
+  // equate to communication errors.)
+  retVal = esp8266.status();
+  if (retVal <= 0)
+  {
+    Serial.print(F("Connecting to "));
+    Serial.println(mySSID);
+    // esp8266.connect([ssid], [psk]) connects the ESP8266
+    // to a network.
+    // On success the connect function returns a value >0
+    // On fail, the function will either return:
+    //  -1: TIMEOUT - The library has a set 30s timeout
+    //  -3: FAIL - Couldn't connect to network.
+    retVal = esp8266.connect(mySSID, myPSK);
+    if (retVal < 0)
+    {
+      Serial.println(F("Error connecting"));
+      errorLoop(retVal);
+    }
+  }
+}
+
+void displayConnectInfo()
+{
+  char connectedSSID[24];
+  memset(connectedSSID, 0, 24);
+  // esp8266.getAP() can be used to check which AP the
+  // ESP8266 is connected to. It returns an error code.
+  // The connected AP is returned by reference as a parameter.
+  int retVal = esp8266.getAP(connectedSSID);
+  if (retVal > 0)
+  {
+    Serial.print(F("Connected to: "));
+    Serial.println(connectedSSID);
+  }
+
+  // esp8266.localIP returns an IPAddress variable with the
+  // ESP8266's current local IP address.
+  IPAddress myIP = esp8266.localIP();
+  Serial.print(F("My IP: ")); Serial.println(myIP);
+}
+
+void getWeather()
+{
+  if (numRuns <= maxRuns) {
+    Serial.println("Running GetWeatherByAddress - Run #" + String(numRuns++));
+
+    delay(50);
+
+    TembooChoreo GetWeatherByAddressChoreo(client);
+
+    // Invoke the Temboo client
+    GetWeatherByAddressChoreo.begin();
+
+    // Set Temboo account credentials
+    GetWeatherByAddressChoreo.setAccountName(TEMBOO_ACCOUNT);
+    GetWeatherByAddressChoreo.setAppKeyName(TEMBOO_APP_KEY_NAME);
+    GetWeatherByAddressChoreo.setAppKey(TEMBOO_APP_KEY);
+
+    // Set Choreo inputs
+    String AddressValue = "edmonton, AB";
+    GetWeatherByAddressChoreo.addInput("Address", AddressValue);
+    String UnitsValue = "c";
+    GetWeatherByAddressChoreo.addInput("Units", UnitsValue);
+
+    // Identify the Choreo to run
+    GetWeatherByAddressChoreo.setChoreo("/Library/Yahoo/Weather/GetWeatherByAddress");
+
+    // Run the Choreo; when results are available, print them to serial
+    GetWeatherByAddressChoreo.run();
+
+    delay(50);
+    
+    while(GetWeatherByAddressChoreo.available()) {
+      delay(50);
+      char c = GetWeatherByAddressChoreo.read();
+      Serial.print(c);
+    }
+    GetWeatherByAddressChoreo.close();
+  }
+}
+
+// errorLoop prints an error code, then loops forever.
+void errorLoop(int error)
+{
+  Serial.print(F("Error: ")); Serial.println(error);
+  Serial.println(F("Looping forever."));
+  for (;;)
+    ;
+}
+
+// serialTrigger prints a message, then waits for something
+// to come in from the serial port.
+void serialTrigger(String message)
+{
+  Serial.println();
+  Serial.println(message);
+  Serial.println();
+  while (!Serial.available())
+    ;
+  while (Serial.available())
+    Serial.read();
+}
