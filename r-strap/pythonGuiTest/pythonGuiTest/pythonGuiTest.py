d2b79a770db2857c0abd5e5a855393a6e6394e36
Phil Kloc <philipgkloc@gmail.com>
2017-02-03 19:03:56 +0000

    Update to handle non-complete receipts.

--- r-strap/pythonGuiTest/pythonGuiTest/pythonGuiTest.py
+++ r-strap/pythonGuiTest/pythonGuiTest/pythonGuiTest.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from serial import *
+from builtins import print
 
 serial_port = 'COM3'
 baud_rate = 9600
@@ -30,25 +31,32 @@ log.pack()
 log.config(yscrollcommand=scrollbar.set)
 scrollbar.config(command = log.yview)
 
+serInput = ''
+
 def readSerial():
-	try:
-		global ser
+    try:
+        global ser
+        global serInput
+        
+        if ser.is_open:
+            serInput = serInput + ser.readline().decode('utf-8')
 
-		if ser.is_open:
-			serInput = ser.readline().decode('utf-8')
+            #self.serial.read(self.serial.in_waiting or 1)
 
-			#self.serial.read(self.serial.in_waiting or 1)
+            #print(serInput) if serInput != '' else None
+            if serInput.endswith('\n'):
+                log.insert('0.0', serInput)
+                serInput = ''
 
-			#print(serInput) if serInput != '' else None
-			log.insert('0.0', serInput) if serInput != '' else None
-		else:
-			ser = serial.Serial('COM3', 9600, timeout=0)
-	except:
-		print('No data')
-		
-		if ser.is_open:
-			ser.close()
-	rootWindow.after(100, readSerial)
+        else:
+            ser = serial.Serial('COM3', 9600, timeout=0)
+    except:
+        print('No data')
+
+        if ser.is_open:
+            ser.close()
+
+    rootWindow.after(100, readSerial)
 
 rootWindow.after(100, readSerial)
 
